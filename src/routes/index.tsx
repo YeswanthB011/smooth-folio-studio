cfb7e57cc0536e710ab771da2480f6be37415797
Yeswanth <104953452+YeswanthB011@users.noreply.github.com>
2026-05-31 11:47:13 +0530

    Update resume link in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -49,7 +49,7 @@ const links = [
   { label: "GitHub", href: "https://github.com/YeswanthB011" },
   { label: "LinkedIn", href: "https://www.linkedin.com/in/yeswanth-b-8748aa1a7/" },
   { label: "Instagram", href: "https://www.instagram.com/yeswanth_11.1/" },
-  { label: "Resume", href: `/Yeswanth_Resume.pdf` },
+  { label: "Resume", href: `/Yeswanth_Automation Tester_3YOE.pdf` },
 ];
 
 function Index() {
